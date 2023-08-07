06eb07935b3de7f0754ee88b53221f61f2f4bcc0
Erik Král <ekral@utb.cz>
2023-08-07 12:25:58 +0200

    Edit primary key

--- Utb.PizzaKiosk.Models/Pizza.cs
+++ Utb.PizzaKiosk.Models/Pizza.cs
@@ -2,7 +2,7 @@
 {
     public class Pizza
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
         public required string Name { get; init; }
         public required string Description { get; init; }
     }
