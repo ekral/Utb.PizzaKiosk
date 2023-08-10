c2bb87f83f9d6e3179dda7d2957578a971998845
Erik Král <ekral@utb.cz>
2023-08-10 14:21:38 +0200

    Edit class diagram

--- Utb.PizzaKiosk.Models/Pizza.cs
+++ Utb.PizzaKiosk.Models/Pizza.cs
@@ -6,4 +6,15 @@
         public required string Name { get; init; }
         public required string Description { get; init; }
     }
+
+    class Skupina
+    {
+        public string Nazev { get; set; }
+        public int Id { get; set; }
+    }
+
+    class Student
+    {
+        public Skupina Skupina { get; set; }
+    }
 }
